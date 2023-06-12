bebe6fcba2da8e872a85527562254931fa3407ea
Nicholas <nicholas_smilovic@hotmail.com>
2023-06-12 11:20:32 -0400

    googlesigning implementation

--- src/app/item/items.component.ts
+++ src/app/item/items.component.ts
@@ -2,6 +2,15 @@ import { Component, OnInit } from '@angular/core'
 
 import { Item } from './item'
 import { ItemService } from './item.service'
+import { GoogleSignin } from '@nativescript/google-signin';
+
+import { registerElement } from '@nativescript/angular';
+registerElement('GoogleSignInButton', () => require('@nativescript/google-signin').GoogleSignInButton);
+
+import { firebase } from '@nativescript/firebase-core'
+// import '@nativescript/firebase-messaging'; // only needs to be imported 1x
+// import '@nativescript/firebase-database'; // only needs to be imported 1x
+import '@nativescript/firebase-auth'; // only needs to be imported 1x
 
 @Component({
   selector: 'ns-items',
@@ -14,5 +23,39 @@ export class ItemsComponent implements OnInit {
 
   ngOnInit(): void {
     this.items = this.itemService.getItems()
+
+
+    firebase().initializeApp().then(() => {
+      console.warn('🔥 Firebase initialized');
+    }).catch(error => {
+      console.error('🔥 Firebase initialization error: ', error)
+    });
+
+  }
+  loginWithGoogle() {
+    GoogleSignin.signIn()
+      .then((user) => {
+        console.log("GoogleSignin signin success")
+          // const credential = GoogleAuthProvider.credential(user.idToken, user.accessToken);
+
+          // firebase()
+          //     .auth()
+          //     .signInWithCredential(credential)
+          //     .then((userCredential: UserCredential) => {
+          //         this.getToken(userCredential.user).then((token: string) => {
+          //             console.log('FirebaseService::loginGoogle(), token: ' + JSON.stringify(token));
+          //             resolve(token);
+          //         });
+          //     })
+          //     .catch((error) => {
+          //         console.log("FirebaseService::loginGoogle::signInWithCredential " + error);
+
+          //         reject(error);
+          //     });
+      })
+      .catch((error) => {
+          // console.log("FirebaseService::loginGoogle::GoogleSignin.signIn " + error);
+          // reject(error);
+      });
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,14 @@
 import { platformNativeScript, runNativeScriptAngularApp } from '@nativescript/angular';
 
 import { AppModule } from './app/app.module';
+import { GoogleSignin } from '@nativescript/google-signin';
 
+GoogleSignin.configure({})
+.then(() => {
+  console.warn('GoogleSignin configured');
+}).catch(error => {
+  console.error('GoogleSignin configuration error: ', error);
+});
 runNativeScriptAngularApp({
   appModuleBootstrap: () => platformNativeScript().bootstrapModule(AppModule),
 });
